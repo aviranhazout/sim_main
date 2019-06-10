f6fb8c4a3b3d0f1f62abb8c8c92e607585804aa6
aviranhazout <aviranhazout@campus.technion.ac.il>
2019-06-10 22:35:08 +0300

    sadxcv

--- core_api.c
+++ core_api.c
@@ -5,16 +5,64 @@
 
 #include <stdio.h>
 
+#define NUM_REGS 8
 
-
+int num_insts = 0;
+int num_cycles_blocked = 0;
+int num_cycles_finegrained = 0;
+tcontext* finegrained_thread_regs;
+tcontext* blocked_thread_regs;
 
 Status Core_blocked_Multithreading(){
 	return Success;
 }
+Status init (tcontext* contexts, int* pcs, bool* thread_finished)
+{
+    int threads_num = Get_thread_number();
+    int* pcs = (int*)malloc(threads_num * sizeof(int));
+    if (pcs == NULL)
+        return Failure;
+    v = (tcontext*)malloc(threads_num * sizeof(tcontext));
+    if (contexts == NULL)
+        return Failure;
+    thread_finished = (bool*)malloc(threads_num * sizeof(bool));
+    for(int i = 0; i < threads_num; i++)
+    {
+        thread_finished[i] = false;
+        pcs[i] = 0;
+        for (int j = 0; j < NUM_REGS; j++)
+            contexts[i][j] = 0;
+    }
+    return Success;
+}
+
+Status Core_fineGrained_Multithreading()
+{
+    int threads_num = Get_thread_number();
+    int* pcs;
+    bool* thread_finished;
+    if (init(blocked_thread_regs, pcs, thread_finished) == Failure)
+        return Failure;
 
 
-Status Core_fineGrained_Multithreading(){
+    Instuction inst;
+    int threads_done = 0;
+    while(threads_done < threads_num)
+    {
 
+
+
+
+
+
+
+
+
+        threads_done = 0;
+        for(int i = 0; i < threads_num; i++){
+            threads_done += thread_finished[i];
+        }
+    }
 	return Success;
 }
 
