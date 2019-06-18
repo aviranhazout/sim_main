8faac1320d9359b52198eeddb099e1fca23dfaae
nater <nate.tehnion@gmail.com>
2019-06-18 12:07:59 +0300

    fine_grained passes first three tests

--- core_api.c
+++ core_api.c
@@ -10,6 +10,9 @@
 enum multi_thread_type{FINE_GRAINED, BLOCKED};
 
 
+//TODO ********FOR TESTING ***********
+int* copyReg[3][NUM_REGS];
+
 int num_insts_blocked = 0;
 int num_cycles_blocked = 0;
 tcontext* blocked_regs;
@@ -17,6 +20,15 @@ tcontext* blocked_regs;
 int num_insts_finegrained = 0;
 int num_cycles_finegrained = 0;
 tcontext* finegrained_regs;
+
+//*********FOR TESTING*************
+void copy_reg(tcontext copyReg[], int reg[], int Threads){
+    for (int i = 0; i < NUM_REGS; ++i) {
+        copyReg[Threads].reg[i] = reg[i];
+    }
+}
+
+
 
 
 Status init (tcontext** contexts, int** pcs, bool** thread_finished, int** wait_to_memory, int* load_latency, int* store_latency)
@@ -260,6 +272,7 @@ Status Core_blocked_Multithreading()
     return Success;
 }
 
+/*
 Status _digest_opcode(enum multi_thread_type multi_type, int* thread_idx, int pcs[], bool thread_finished[],
         int *wait_to_memory, int load_latency, int store_latency){
     Instuction inst;
@@ -283,7 +296,7 @@ Status _digest_opcode(enum multi_thread_type multi_type, int* thread_idx, int pc
             else
                 num_insts_blocked++;
 
-            pcs[*thread_idx] += 4;
+            pcs[*thread_idx]++;
             break;
         case CMD_SUB :
             finegrained_regs[*thread_idx].reg[inst.dst_index] = finegrained_regs[*thread_idx].reg[inst.src1_index]
@@ -293,7 +306,7 @@ Status _digest_opcode(enum multi_thread_type multi_type, int* thread_idx, int pc
             else
                 num_insts_blocked++;
 
-            pcs[*thread_idx] += 4;
+            pcs[*thread_idx]++;
             break;
         case CMD_ADDI :
             finegrained_regs[*thread_idx].reg[inst.dst_index] = finegrained_regs[*thread_idx].reg[inst.src1_index]
@@ -303,7 +316,7 @@ Status _digest_opcode(enum multi_thread_type multi_type, int* thread_idx, int pc
             else
                 num_insts_blocked++;
 
-            pcs[*thread_idx] += 4;
+            pcs[*thread_idx]++;
             break;
         case CMD_SUBI :
             finegrained_regs[*thread_idx].reg[inst.dst_index] = finegrained_regs[*thread_idx].reg[inst.src1_index]
@@ -313,7 +326,7 @@ Status _digest_opcode(enum multi_thread_type multi_type, int* thread_idx, int pc
             else
                 num_insts_blocked++;
 
-            pcs[*thread_idx] += 4;
+            pcs[*thread_idx]++;
             break;
         case CMD_LOAD :
             if (wait_to_memory[*thread_idx] == 0) {//start the instruction
@@ -334,7 +347,7 @@ Status _digest_opcode(enum multi_thread_type multi_type, int* thread_idx, int pc
                     context_switch(*thread_idx, thread_finished);
             } else if (wait_to_memory[*thread_idx] == -1) {//end the instruction
                 wait_to_memory[*thread_idx] = 0;
-                pcs[*thread_idx] += 4;
+                pcs[*thread_idx]++;
             }
             //still waiting or completed
             break;
@@ -357,7 +370,7 @@ Status _digest_opcode(enum multi_thread_type multi_type, int* thread_idx, int pc
                     context_switch(*thread_idx, thread_finished);
             } else if (wait_to_memory[*thread_idx] == -1) {//end the instruction
                 wait_to_memory[*thread_idx] = 0;
-                pcs[*thread_idx] += 4;
+                pcs[*thread_idx]++;
             }
 
             break;
@@ -379,7 +392,7 @@ Status _digest_opcode(enum multi_thread_type multi_type, int* thread_idx, int pc
     update_latencies(wait_to_memory);
     return Success;
 }
-
+*/
 
 Status Core_fineGrained_Multithreading() {
     int threads_num = Get_thread_number();
@@ -391,11 +404,132 @@ Status Core_fineGrained_Multithreading() {
     int *wait_to_memory;
     int load_latency;
     int store_latency;
-    if (init(&finegrained_regs, &pcs, &thread_finished, &wait_to_memory, &load_latency, &store_latency) == Failure)
+
+
+
+
+    //init
+    pcs = (int*)malloc(threads_num * sizeof(int));
+    if (pcs == NULL)
         return Failure;
+    finegrained_regs = (tcontext*)malloc(threads_num * sizeof(tcontext));
+    if (finegrained_regs == NULL)
+        return Failure;
+    wait_to_memory = (int*)malloc(threads_num * sizeof(int));
+    if (wait_to_memory == NULL)
+        return Failure;
+    thread_finished = (bool*)malloc(threads_num * sizeof(bool));
+    int latencies[2];
+    Mem_latency(latencies);
+    load_latency = latencies[0];
+    store_latency = latencies[1];
+    for(int i = 0; i < threads_num; i++)
+    {
+        thread_finished[i] = false;
+        wait_to_memory[i] = 0;
+        pcs[i] = 0;
+        for (int j = 0; j < NUM_REGS; j++)
+            finegrained_regs[i].reg[j] = 0;
+    }
+
+    //TODO *********FOR TESTING ********
+    for (int k = 0; k < 3; ++k) {
+        for (int i = 0; i < NUM_REGS; ++i) {
+            copyReg[k][i] = &finegrained_regs[k].reg[i];
+        }
+    }
+    //if (init(finegrained_regs, pcs, thread_finished, wait_to_memory, &load_latency, &store_latency) == Failure)
+     //   return Failure;
     int thread_idx = 0;
     while (!is_done(thread_finished)) {
-        _digest_opcode(FINE_GRAINED, &thread_idx, pcs, thread_finished, wait_to_memory, load_latency, store_latency);
+        Instuction inst;
+
+        SIM_MemInstRead(pcs[thread_idx], &inst, thread_idx);
+        num_cycles_finegrained++;
+        switch (inst.opcode) {
+            case CMD_NOP :
+                //bubble - i think just increase cycle count and if finegrained then switch thread
+                break;
+
+            case CMD_ADD :
+                finegrained_regs[thread_idx].reg[inst.dst_index] = finegrained_regs[thread_idx].reg[inst.src1_index]
+                                                                + finegrained_regs[thread_idx].reg[inst.src2_index_imm];
+                pcs[thread_idx]++;
+                break;
+
+            case CMD_SUB :
+                finegrained_regs[thread_idx].reg[inst.dst_index] = finegrained_regs[thread_idx].reg[inst.src1_index]
+                                                                - finegrained_regs[thread_idx].reg[inst.src2_index_imm];
+                pcs[thread_idx]++;
+                break;
+
+            case CMD_ADDI :
+                finegrained_regs[thread_idx].reg[inst.dst_index] = finegrained_regs[thread_idx].reg[inst.src1_index]
+                                                                    + inst.src2_index_imm;
+                pcs[thread_idx]++;
+                break;
+
+            case CMD_SUBI :
+                finegrained_regs[thread_idx].reg[inst.dst_index] = finegrained_regs[thread_idx].reg[inst.src1_index]
+                                                                    - inst.src2_index_imm;
+                pcs[thread_idx]++;
+                break;
+
+            case CMD_LOAD :
+                if (wait_to_memory[thread_idx] == 0)
+                {//start the instruction
+                    num_insts_finegrained--;
+                    int mem_address;
+                    if (inst.isSrc2Imm)
+                        mem_address = finegrained_regs[thread_idx].reg[inst.src1_index] + inst.src2_index_imm;
+                    else
+                        mem_address = finegrained_regs[thread_idx].reg[inst.src1_index] + finegrained_regs[thread_idx].reg[inst.src2_index_imm];
+                    SIM_MemDataRead(mem_address, &(finegrained_regs[thread_idx].reg[inst.dst_index]));
+                    wait_to_memory[thread_idx] = load_latency; //TODO maybe +1
+                }
+                else if (wait_to_memory[thread_idx] == -1)
+                {//end the instruction
+                    wait_to_memory[thread_idx] = 0;
+                    pcs[thread_idx]++;
+                    num_insts_blocked++;
+                }
+                else
+                {//still waiting
+                    num_insts_finegrained--;
+                }
+                break;
+            case CMD_STORE :
+                if (wait_to_memory[thread_idx] == 0)
+                {//start the instruction
+                    num_insts_finegrained--;
+                    int mem_address;
+                    if (inst.isSrc2Imm)
+                        mem_address = finegrained_regs[thread_idx].reg[inst.dst_index] + inst.src2_index_imm;
+                    else
+                        mem_address = finegrained_regs[thread_idx].reg[inst.dst_index] + finegrained_regs[thread_idx].reg[inst.src2_index_imm];
+                    SIM_MemDataWrite(mem_address, finegrained_regs[thread_idx].reg[inst.src1_index]);
+                    wait_to_memory[thread_idx] = store_latency; //TODO maybe +1
+                }
+                else if (wait_to_memory[thread_idx] == -1)
+                {//end the instruction
+                    wait_to_memory[thread_idx] = 0;
+                    pcs[thread_idx]++;
+                    num_insts_blocked++;
+                }
+                else
+                {//still waiting
+                    num_insts_finegrained--;
+                }
+                break;
+
+            case CMD_HALT :
+                thread_finished[thread_idx] = true;
+                break;
+        }
+        // continue to next thread after command
+        num_insts_finegrained++;
+        thread_idx = next_thread(thread_idx, thread_finished);
+        update_latencies(wait_to_memory);
     }
     return Success;
 }
@@ -404,7 +538,7 @@ double Core_finegrained_CPI(){
     if (num_cycles_finegrained == 0)
         return 0;
 
-    return (num_cycles_finegrained / num_cycles_finegrained);
+    return ((double)num_cycles_finegrained / (double)num_insts_finegrained);
 }
 double Core_blocked_CPI(){
     if (num_cycles_blocked == 0)
@@ -421,6 +555,8 @@ Status Core_blocked_context(tcontext* bcontext,int threadid)
 }
 
 Status Core_finegrained_context(tcontext* finegrained_context,int threadid){
-    finegrained_context = &finegrained_regs[threadid];
+    for (int i = 0; i < NUM_REGS; i++)
+        finegrained_context[threadid].reg[i] = finegrained_regs[threadid].reg[i];
 	return Success;
 }
+
